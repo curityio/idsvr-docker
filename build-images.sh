426eef1f55a5c69f3372a9714b7c0836e95023ad
Nikos Anestos <nikos@curity.io>
2019-09-25 17:29:33 +0200

    feat/master/script to update docker images (#2)
    * don't copy again if directory exists
    
    * added script to re-build images
    
    * fix inline check
    
    * directory -> images
    
    * quote the full url in curl
    
    * unify variables
    
    * spaces
    
    * remove redundant input; silent curl; write errors in stdout

--- build-images.sh
+++ build-images.sh
@@ -1,16 +1,18 @@
-#/bin/bash
+#!/bin/bash
 
-mkdir -p ${VERSION}/ubuntu
-mkdir -p ${VERSION}/centos
-mkdir -p ${VERSION}/stretch
-mkdir -p ${VERSION}/stretch-slim
+if [ ! -d "${VERSION}" ]; then
+  mkdir -p "${VERSION}/ubuntu"
+  mkdir -p "${VERSION}/centos"
+  mkdir -p "${VERSION}/stretch"
+  mkdir -p "${VERSION}/stretch-slim"
 
-cat Dockerfile-ubuntu.template | sed -e "s/{{VERSION}}/$VERSION/g" > ${VERSION}/ubuntu/Dockerfile
-cat Dockerfile-centos.template | sed -e "s/{{VERSION}}/$VERSION/g" > ${VERSION}/centos/Dockerfile
-cat Dockerfile-stretch.template | sed -e "s/{{VERSION}}/$VERSION/g" > ${VERSION}/stretch/Dockerfile
-cat Dockerfile-stretch-slim.template | sed -e "s/{{VERSION}}/$VERSION/g" > ${VERSION}/stretch-slim/Dockerfile
+  sed -e "s/{{VERSION}}/${VERSION}/g" Dockerfile-ubuntu.template > "${VERSION}/ubuntu/Dockerfile"
+  sed -e "s/{{VERSION}}/${VERSION}/g" Dockerfile-centos.template > "${VERSION}/centos/Dockerfile"
+  sed -e "s/{{VERSION}}/${VERSION}/g" Dockerfile-stretch.template > "${VERSION}/stretch/Dockerfile"
+  sed -e "s/{{VERSION}}/${VERSION}/g" Dockerfile-stretch-slim.template > "${VERSION}/stretch-slim/Dockerfile"
+fi
 
-docker build --no-cache -t curity/idsvr:latest -t curity/idsvr:${VERSION} -t curity/idsvr:${VERSION}-ubuntu -t curity/idsvr:${VERSION}-ubuntu18 -t curity/idsvr:${VERSION}-ubuntu18.04 -f ${VERSION}/ubuntu/Dockerfile .
-docker build --no-cache -t curity/idsvr:${VERSION}-centos -t curity/idsvr:${VERSION}-centos7 -f ${VERSION}/centos/Dockerfile .
-docker build --no-cache -t curity/idsvr:${VERSION}-stretch -f ${VERSION}/stretch/Dockerfile .
-docker build --no-cache -t curity/idsvr:${VERSION}-stretch-slim -t curity/idsvr:${VERSION}-slim -f ${VERSION}/stretch-slim/Dockerfile .
+docker build --no-cache -t curity/idsvr:latest -t "curity/idsvr:${VERSION}" -t "curity/idsvr:${VERSION}-ubuntu" -t "curity/idsvr:${VERSION}-ubuntu18" -t "curity/idsvr:${VERSION}-ubuntu18.04" -f "${VERSION}/ubuntu/Dockerfile" .
+docker build --no-cache -t "curity/idsvr:${VERSION}-centos" -t "curity/idsvr:${VERSION}-centos7" -f "${VERSION}/centos/Dockerfile" .
+docker build --no-cache -t "curity/idsvr:${VERSION}-stretch" -f "${VERSION}/stretch/Dockerfile" .
+docker build --no-cache -t "curity/idsvr:${VERSION}-stretch-slim" -t "curity/idsvr:${VERSION}-slim" -f "${VERSION}/stretch-slim/Dockerfile" .
