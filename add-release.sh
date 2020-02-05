e93b343ea33a478db0a7fab556909c880dbabe76
Travis Spencer <travis@curity.io>
2020-02-05 19:44:10 +0100

    Added RHEL Dockerfile template

--- add-release.sh
+++ add-release.sh
@@ -3,6 +3,7 @@
 if [ ! -d "${VERSION}" ]; then
   mkdir -p "${VERSION}/ubuntu"
   mkdir -p "${VERSION}/centos"
+  mkdir -p "${VERSION}/rhel"  
   mkdir -p "${VERSION}/buster"
   mkdir -p "${VERSION}/buster-slim"
 
@@ -11,6 +12,7 @@ if [ ! -d "${VERSION}" ]; then
 
   sed -e "s/{{VERSION}}/${VERSION}/g" Dockerfile-ubuntu.template > "${VERSION}/ubuntu/Dockerfile"
   sed -e "s/{{VERSION}}/${VERSION}/g" Dockerfile-centos.template > "${VERSION}/centos/Dockerfile"
+  sed -e "s/{{VERSION}}/${VERSION}/g" Dockerfile-rhel.template > "${VERSION}/rhel/Dockerfile"  
   sed -e "s/{{VERSION}}/${VERSION}/g" Dockerfile-buster.template > "${VERSION}/buster/Dockerfile"
   sed -e "s/{{VERSION}}/${VERSION}/g" Dockerfile-buster-slim.template > "${VERSION}/buster-slim/Dockerfile"
 fi
