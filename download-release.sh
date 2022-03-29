f092f7c67db8af62b16763c54c45c081979d1855
Nikos Anestos <nikos.anestos@curity.io>
2022-03-29 11:19:29 +0200

    download release will skip re-downloading if the file exists and is OK

--- download-release.sh
+++ download-release.sh
@@ -12,7 +12,6 @@ TOKEN_ENDPOINT=https://login.curity.io/oauth/v2/token
 RELEASE_API=https://releaseapi.curity.io/releases
 SCOPE="release_download release_read"
 
-echo "Downloading version ${VERSION}";
 ACCESS_TOKEN=$(curl -f -s -S -d "grant_type=client_credentials&client_secret=${CLIENT_SECRET}&client_id=${CLIENT_ID}&scope=${SCOPE}" "${TOKEN_ENDPOINT}" | jq -r '.access_token')
 
 if [[ "${ACCESS_TOKEN}" == "null" ]]; then
@@ -20,15 +19,29 @@ if [[ "${ACCESS_TOKEN}" == "null" ]]; then
   exit 1
 fi
 
+RELEASE_HASH=$(curl -f -s -S -H "Authorization: Bearer ${ACCESS_TOKEN}" "${RELEASE_API}/${VERSION}" | jq -r ".\"${ARTIFACT}-sha256-checksum\"")
+
 # Download the release from the release API
 RELEASE_FILENAME="idsvr-${VERSION}-${ARTIFACT}.tar.gz"
-if [ -f $RELEASE_FILENAME ]; then
-  rm "$RELEASE_FILENAME"
+DOWNLOAD_RELEASE=
+if [ -f "$RELEASE_FILENAME" ]; then
+  echo "$RELEASE_FILENAME found"
+  CALCULATED_RELEASE_HASH=$(openssl sha256 "${RELEASE_FILENAME}")
+  if [[ $CALCULATED_RELEASE_HASH != *$RELEASE_HASH* ]]; then
+    echo "$RELEASE_FILENAME hash is NOT OK"
+    rm "$RELEASE_FILENAME"
+  else
+    echo "$RELEASE_FILENAME hash was verified, will skip download"
+    DOWNLOAD_RELEASE=1
+  fi
 fi
+
+if [ -z $DOWNLOAD_RELEASE ]; then
+  echo "Downloading ${RELEASE_FILENAME}";
   curl -f -s -S -H "Authorization: Bearer ${ACCESS_TOKEN}" "${RELEASE_API}/${VERSION}/${ARTIFACT}-release" > "${RELEASE_FILENAME}"
+fi
 
 # Verify hash of downloaded file
-RELEASE_HASH=$(curl -f -s -S -H "Authorization: Bearer ${ACCESS_TOKEN}" "${RELEASE_API}/${VERSION}" | jq -r ".\"${ARTIFACT}-sha256-checksum\"")
 CALCULATED_RELEASE_HASH=$(openssl sha256 "${RELEASE_FILENAME}")
 if [[ $CALCULATED_RELEASE_HASH != *$RELEASE_HASH* ]]; then
   echo "Release hash verification failed, expecting $RELEASE_HASH and got $CALCULATED_RELEASE_HASH for $RELEASE_FILENAME"
