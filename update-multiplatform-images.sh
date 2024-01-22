cc9167bb210e6c0e0e25e887b3844dd9d6dd48a5
Nikos Anestos <nikos.anestos@curity.io>
2024-01-22 15:33:49 +0100

    Update build process for bookworm and centos9 for version 9

--- update-multiplatform-images.sh
+++ update-multiplatform-images.sh
@@ -8,37 +8,47 @@ IMAGE_BASE="curity.azurecr.io/curity/idsvr"
 [[ -z "${CLIENT_ID}" ]] && echo "CLIENT_ID not set" >&2 && exit 1;
 [[ -z "${CLIENT_SECRET}" ]] && echo "CLIENT_SECRET not set" >&2 && exit 1;
 
-UBUNTU_18=ubuntu:18.04 # Keep for version 7.x
 UBUNTU_22=ubuntu:22.04
-CENTOS_8=quay.io/centos/centos:stream8
-BUSTER=buildpack-deps:buster
-BUSTER_SLIM=debian:buster-slim
+CENTOS_8=quay.io/centos/centos:stream8 # Keep for version 8.x
+CENTOS_9=quay.io/centos/centos:stream9
+BUSTER=buildpack-deps:buster # Keep for version 8.x
+BUSTER_SLIM=debian:buster-slim # Keep for version 8.x
+BOOKWORM=debian:bookworm
+BOOKWORM_SLIM=debian:bookworm-slim
 
 # Pull x86 base images once to avoid pull limit in dockerhub
-docker pull "$UBUNTU_18" --platform linux/amd64
 docker pull "$UBUNTU_22" --platform linux/amd64
 docker pull "$CENTOS_8" --platform linux/amd64
+docker pull "$CENTOS_9" --platform linux/amd64
 docker pull "$BUSTER" --platform linux/amd64
 docker pull "$BUSTER_SLIM" --platform linux/amd64
+docker pull "$BOOKWORM" --platform linux/amd64
+docker pull "$BOOKWORM_SLIM" --platform linux/amd64
 
-UBUNTU_18_X86_LAST_LAYER_ID=$(docker inspect "${UBUNTU_18}" | jq ".[0].RootFS.Layers[-1]"); export UBUNTU_18_X86_LAST_LAYER_ID
 UBUNTU_X86_LAST_LAYER_ID=$(docker inspect "${UBUNTU_22}" | jq ".[0].RootFS.Layers[-1]"); export UBUNTU_X86_LAST_LAYER_ID
 CENTOS_X86_LAST_LAYER_ID=$(docker inspect "${CENTOS_8}" | jq ".[0].RootFS.Layers[-1]"); export CENTOS_X86_LAST_LAYER_ID
+CENTOS9_X86_LAST_LAYER_ID=$(docker inspect "${CENTOS_9}" | jq ".[0].RootFS.Layers[-1]"); export CENTOS9_X86_LAST_LAYER_ID
 BUSTER_X86_LAST_LAYER_ID=$(docker inspect "${BUSTER}" | jq ".[0].RootFS.Layers[-1]"); export BUSTER_X86_LAST_LAYER_ID
 BUSTER_SLIM_X86_LAST_LAYER_ID=$(docker inspect "${BUSTER_SLIM}" | jq ".[0].RootFS.Layers[-1]"); export BUSTER_SLIM_X86_LAST_LAYER_ID
+BOOKWORM_X86_LAST_LAYER_ID=$(docker inspect "${BOOKWORM}" | jq ".[0].RootFS.Layers[-1]"); export BOOKWORM_X86_LAST_LAYER_ID
+BOOKWORM_SLIM_X86_LAST_LAYER_ID=$(docker inspect "${BOOKWORM_SLIM}" | jq ".[0].RootFS.Layers[-1]"); export BOOKWORM_SLIM_X86_LAST_LAYER_ID
 
 # Pull ARM base images once to avoid pull limit in dockerhub
-docker pull $UBUNTU_18 --platform linux/arm64
-docker pull $UBUNTU_22 --platform linux/arm64
-docker pull $CENTOS_8 --platform linux/arm64
-docker pull $BUSTER --platform linux/arm64
-docker pull $BUSTER_SLIM --platform linux/arm64
+docker pull "$UBUNTU_22" --platform linux/arm64
+docker pull "$CENTOS_8" --platform linux/arm64
+docker pull "$CENTOS_9" --platform linux/arm64
+docker pull "$BUSTER" --platform linux/arm64
+docker pull "$BUSTER_SLIM" --platform linux/arm64
+docker pull "$BOOKWORM" --platform linux/arm64
+docker pull "$BOOKWORM_SLIM" --platform linux/arm64
 
-UBUNTU_18_ARM_LAST_LAYER_ID=$(docker inspect "${UBUNTU_18}" | jq ".[0].RootFS.Layers[-1]"); export UBUNTU_18_ARM_LAST_LAYER_ID
 UBUNTU_ARM_LAST_LAYER_ID=$(docker inspect "${UBUNTU_22}" | jq ".[0].RootFS.Layers[-1]"); export UBUNTU_ARM_LAST_LAYER_ID
 CENTOS_ARM_LAST_LAYER_ID=$(docker inspect "${CENTOS_8}" | jq ".[0].RootFS.Layers[-1]"); export CENTOS_ARM_LAST_LAYER_ID
+CENTOS9_ARM_LAST_LAYER_ID=$(docker inspect "${CENTOS_9}" | jq ".[0].RootFS.Layers[-1]"); export CENTOS9_ARM_LAST_LAYER_ID
 BUSTER_ARM_LAST_LAYER_ID=$(docker inspect "${BUSTER}" | jq ".[0].RootFS.Layers[-1]"); export BUSTER_ARM_LAST_LAYER_ID
 BUSTER_SLIM_ARM_LAST_LAYER_ID=$(docker inspect "${BUSTER_SLIM}" | jq ".[0].RootFS.Layers[-1]"); export BUSTER_SLIM_ARM_LAST_LAYER_ID
+BOOKWORM_ARM_LAST_LAYER_ID=$(docker inspect "${BOOKWORM}" | jq ".[0].RootFS.Layers[-1]"); export BOOKWORM_ARM_LAST_LAYER_ID
+BOOKWORM_SLIM_ARM_LAST_LAYER_ID=$(docker inspect "${BOOKWORM_SLIM}" | jq ".[0].RootFS.Layers[-1]"); export BOOKWORM_SLIM_ARM_LAST_LAYER_ID
 
 docker buildx create --use
 while IFS= read -r VERSION
