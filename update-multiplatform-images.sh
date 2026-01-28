f570562b4926f9b1aba7e7858fb46a0a10c2a0ed
Nikos Anestos <nikos.anestos@curity.io>
2026-01-28 11:30:40 +0100

    add maxdepth on find

--- update-multiplatform-images.sh
+++ update-multiplatform-images.sh
@@ -44,7 +44,7 @@ docker buildx create --use
 while IFS= read -r VERSION
 do
   VERSION=${VERSION} "$D"/build-multiplatform-images.sh
-done < <(find -- * -name "*.[0-9].[0-9]*" -type d | sort -Vr)
+done < <(find -- * -name "*.[0-9].[0-9]*" -type d -maxdepth 1 | sort -Vr)
 
 # Delete stopped containers and images
 docker buildx prune -af && docker buildx rm
